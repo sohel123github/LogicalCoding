5365f53d6a4bb041d01a0fef971773c1c8cbdb74
Sohel <patvekarsuhel@gmail.com>
2023-10-05 20:31:58 +0530

    Added new code

--- LogicalCoding/src/com/codeprep/Code.java
+++ LogicalCoding/src/com/codeprep/Code.java
@@ -1,11 +1,13 @@
 package com.codeprep;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 public class Code {
 	public static void main(String[] args) {
-		System.out.println("Just Checking..");
 		
 //		1. (Reverse string + Palindrome or not)	
-			/*String name="Sohel";
+		/*	String name="Sohel";
 			int l=name.length();
 			String rev="";
 			for(int i=l-1; i>=0; i--) {
@@ -16,8 +18,7 @@ public class Code {
 				System.out.println("Palindrome Sring");
 			}else {
 				System.out.println("Not a Palindrome Sring");
-			}*/
-			
+			}*/			
 	//  2. (Palindrome number or not)
 			/*
 			int no = 121;
@@ -32,7 +33,7 @@ public class Code {
 				System.out.println(no+ " Palindrome No");
 			}else {
 				System.out.println(no+ " Not a Palindrome No");
-			}*/
+			}	*/
 			
 			
 //			(Palindrome number or not) + (Scanner class)
@@ -169,9 +170,168 @@ public class Code {
 			System.out.println(al);
 			System.out.println(al1);	*/
 
+//		11.	Swap Two Numbers-
+//			(By using third variable-)
 			
+	/*	int a = 10;
+		int b = 20;
+		int t;
+		System.out.println("Before Swapping");
+		System.out.println("a "+a);
+		System.out.println("b "+b);
+		
+		t=a;
+		a=b;
+		b=t;
+		
+		System.out.println("After Swapping");
+		System.out.println("a "+a);
+		System.out.println("b "+b);		*/
+		
+		
+//			(Without using third variable-)
+		
+	/*	int a = 100;
+		int b = 200;
+		
+		System.out.println("Before swapping");
+		System.out.println("a "+a);
+		System.out.println("b "+b);
+		
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		
+		System.out.println("After Swapping");
+		System.out.println("a "+a);
+		System.out.println("b "+b);	*/
 			
+//		12. find maximum and minimum value in array-
+//			Maximum Value-
+		/*	int[] a = {4,1,3,2,9};
 			
+			int max = a[0];
+			for(int i = 1; i<a.length; i++) {
+				if(a[i] > max) {
+					max = a[i];
+					System.out.println("Maximum value- "+max);
+				}
+			}	*/
+		
+//			Minimum Value-
+		/*	int[] a = {9,14,23,16,2};
+			int min  = a[0];
+			
+			for(int i = 1; i<a.length; i++) {
+				if(a[i] < min) {
+					min = a[i];
+					System.out.println("Minimum Value is "+min);
+				}
+			}	*/	
+		
+//		13.	Find second largest value in array-
+		
+		/*	int[] a= {6,8,2,4,3,1,5,7,9};
+			int temp;
+			for(int i=0; i<a.length; i++) {
+				for(int j=i+1; j<a.length; j++) {
+					if(a[i] < a[j]) {
+						temp=a[i];
+						a[i]=a[j];
+						a[j]=temp;
+					}
+				}
+			}
+			
+			System.out.println("Second largest element is- "+a[1]); */	
+		
+//		14.	Find second smallest value in array-
+		
+	/*	int[] a = {6,8,19,3,10,12,4};
+		int temp;
+		for(int i = 0; i<a.length; i++) {
+			for(int j = i+1; j<a.length; j++) {
+				if(a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		System.out.println("Second smallest element- "+a[1]); */
+		
+//		15. Find kth largest and smallest element in an array-
+		
+//			(kth largest element in array)-
+		/*	int[] a= {9,8,15,10,2,61};
+			int k = 4;
+			for(int i=0; i<a.length; i++) {
+				for(int j=i+1; j<a.length; j++) {
+					if(a[i] < a[j]) {
+						int temp = a[i];
+						a[i] = a[j];
+						a[j] = temp;
+					}
+				}
+				if(i == k) {
+					System.out.println(a[i]);
+				}
+			}	*/
+			
+//			(kth smallest element in array)-
+		/*	int[] a= {8,9,15,6,10,99,13,17};
+			int k = 6;
+			
+			for(int i=0; i<a.length; i++) {
+				for(int j=i+1; j<a.length; j++) {
+					if(a[i] > a[j]) {
+						int temp = a[i];
+						a[i] = a[j];
+						a[j] = temp;
+					}
+				}
+				if(i == k) {
+					System.out.println("Kth smallest element is-"+a[i]);
+				}
+			}	*/
+		
+//		16.	Find Duplicate element in an array-
+//			i]  Brute Force Method-
+//			ii] Set Interface method- (Best Method)
+		/*	int[] a= {8,9,10,61,9,95,51,10,11,8,61};
+			
+			Set<Integer> s = new HashSet<>();
+			
+			for(int no:a) {
+				if(s.add(no) == false) {
+					System.out.println(no+"");
+				}
+			}	*/
+		
+//			iii]Using HashTable-
+			
+//		17. Find first duplicate element in an array-
+		/*	int[] a = {6,5,3,2,5,1,2,4};
+			
+			int temp = -1;
+			HashSet<Integer> hs = new HashSet<>();
+			
+			for(int i=a.length-1; i>=0; i--) {
+				if(hs.contains(a[i])) {
+					temp = a[i];
+				}else {
+					hs.add(a[i]);
+				}
+			}
+			if(temp != -1) {
+				System.out.println("First Duplicate element is- "+temp);
+			}else {
+				System.out.println("Duplicate element not found");
+			}	*/
+			
+//		18.	Find first element that appears once in an array 
+//			where every other element appears twice in java
+		
 			
 			
 			
@@ -183,12 +343,7 @@ public class Code {
 			
 			
 //			just for practice-
-			
-			
-			
-			
-			
-			
+
 			
 			
 			
